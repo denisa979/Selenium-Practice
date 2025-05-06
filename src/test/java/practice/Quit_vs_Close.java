1d847dab603f1562b62bdf2528b79e52f24a33f6
denisa <dmutapcic79@hotmail.com>
2025-05-05 23:11:20 -0400

    practice

---
+++ src/test/java/practice/Quit_vs_Close.java
@@ -0,0 +1,58 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Quit_vs_Close {
+
+    	public static void main(String[] args) {
+
+   /*
+        	// 1. open browser(chrome, ff, safari, edge)
+        	WebDriver driver = new ChromeDriver();//123
+		// 2. enter url
+		driver.get("https://www.google.com");//123
+		// 3. get the page title
+		String title = driver.getTitle();//123
+		System.out.println("page title:" + title);
+
+		// 4. get url
+		String url = driver.getCurrentUrl();//123
+		System.out.println("page url:" + url);
+
+    	// 5. close the browser
+		driver.quit();//123
+
+		//sid=null
+
+		driver = new ChromeDriver();//456
+		driver.get("https://www.google.com");//456
+		System.out.println(driver.getTitle());//456
+
+    */
+
+		// close:
+		// 1. open browser(chrome, ff, safri, edge)
+		WebDriver driver = new ChromeDriver();// 123
+		// 2. enter url
+		driver.get("https://www.google.com");// 123
+		// 3. get the page title
+		String title = driver.getTitle();// 123
+		System.out.println("page title:" + title);
+
+		// 4. get url
+		String url = driver.getCurrentUrl();// 123
+		System.out.println("page url:" + url);
+
+		// 5. close the browser
+		driver.close();//123
+
+		//sid=invalid//NoSuchSessionException: invalid session id
+
+		driver = new ChromeDriver();//456
+		driver.get("https://www.google.com");//456
+		System.out.println(driver.getTitle());//456
+
+
+	}
+}
---
+++ src/test/java/practice/SendKeys.java
@@ -0,0 +1,25 @@
+package practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ElementUtil;
+import utilities.WebDriverBasic;
+
+public class SendKeys {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+
+        driver = new ChromeDriver();//123
+		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+
+        By email = By.id("input-email");
+		By password = By.id("input-password");
+
+		ElementUtil elUtil = new ElementUtil((WebDriver) driver);
+		elUtil.doSendKeys(email, "naveen@gmail.com");
+		elUtil.doSendKeys(password, "naveen@123");
+    }
+}
